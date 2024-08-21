ae4f4252909e51c4c463f312aa6e8fe0b27216a7
martin0995 <martin@papermoon.io>
2024-08-21 14:26:05 +0200

    updated tests to pass

---
+++ test/Placeholder.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+
+contract PlaceholderTest is Test {
+    function testPlaceholder() public {
+        assertTrue(true); // This test will always pass
+    }
+}
