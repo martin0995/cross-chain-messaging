cde410f28be48c68c37c38934d5c7fb2c43f7713
martin0995 <martin@papermoon.io>
2024-08-22 13:13:02 +0200

    updated placeholder test

--- test/Placeholder.t.sol
+++ test/Placeholder.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 import "forge-std/Test.sol";
 
 contract PlaceholderTest is Test {
-    function testPlaceholder() public {
+    function testPlaceholder() pure public {
         assertTrue(true); // This test will always pass
     }
 }
